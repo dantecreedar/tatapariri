5d522ab7fc1213c8018dabcc2084be0145779bb0
DanteCreed <cactus.studio.ar@gmail.com>
2023-12-28 21:03:52 -0300

    second tatapiriri

--- src/components/shared/navbar/item/item.jsx
+++ src/components/shared/navbar/item/item.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
 
+// eslint-disable-next-line react/prop-types
 const Item = ({ item }) => {
   return (
     <>
-      <li>{
+      <li className="">{
+        // eslint-disable-next-line react/prop-types
         item.status ?
-        <a href={item.url} className="block py-2 px-3 text-white bg-black rounded md:bg-transparent md:text-white-700 md:p-0 md:dark:text-white-500" aria-current="page"><i className={item.icon} />  {item.name}
+        // eslint-disable-next-line react/prop-types
+        <a href={item.url} className="block py-2 px-3 text-white bg-black  md:bg-transparent md:text-white-700 md:p-0 md:dark:text-white-500" aria-current="page"><i className={item.icon} />  {item.name}
         </a>
-        : <a href={item.url} className="isDisabled block py-2 px-3 text-white bg-black rounded md:bg-transparent md:text-white-700 md:p-0 md:dark:text-white-500" aria-current="page"><i className={item.icon} />  {item.name}
+        // eslint-disable-next-line react/prop-types
+        : <a href={item.url} className="isDisabled block py-2 px-3 text-white bg-black md:bg-transparent md:text-white-700 md:p-0 md:dark:text-white-500" aria-current="page"><i className={item.icon} />  {item.name}
         </a>
         }
       </li>
--- src/components/shared/navbar/navbar.jsx
+++ src/components/shared/navbar/navbar.jsx
@@ -1,14 +1,16 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./navbar.scss";
 import { items } from "../../items";
 import NavbarBtn from "./navbarbtn/navbarbtn";
 import Item from "./item/item";
 import CotizationBanner from "../../services/cotizationBanner/cotizationBanner";
+import logo from "../../../assets/goop.png"
 
 
 
 const Navbar = () => {
 
+    // eslint-disable-next-line no-unused-vars
     let [coins, setCoins] = useState(0);
 
     return (
@@ -17,20 +19,20 @@ const Navbar = () => {
                 <CotizationBanner />
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                        <img src="https://flowbite.com/docs/images/logo.svg" className="h-6" alt="Flowbite Logo" />
+                        <img src={logo} className="h-6" alt="Flowbite Logo" />
                         <span className="self-center text-2x1 font-semibold whitespace-nowrap dark:text-white">Cooperativa Tatá Pariri</span>
                     </a>
                     <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                         <NavbarBtn coins={coins} />
-                        <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+                        {/* <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
                             <span className="sr-only">Open main menu</span>
                             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
                             </svg>
-                        </button>
+                        </button> */}
                     </div>
-                    <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-                        <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <div className="items-center justify-between w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+                        <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium  border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                             {
                                 items.map((item, idx) => {
                                     return (<Item item={item} key={idx} />)
